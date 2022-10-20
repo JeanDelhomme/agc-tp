69c8a320171ca5a90e04d44fbecda4ccce4fabc4
root <root@DESKTOP-MH82SGB.localdomain>
2022-10-20 15:36:58 +0200

    Everything is done.

--- agc/agc.py
+++ agc/agc.py
@@ -71,22 +71,165 @@ def get_arguments():
     return parser.parse_args()
 
 def read_fasta(amplicon_file, minseqlen):
-    pass
+
+    """
+    Opens a fasta file and reads its sequences.
+
+    Parameters
+    ----------
+        amplicon_file : str
+            The name of the fastq file.
+    Returns
+    -------
+        yield
+        A yield containing the sequences.
+    """
+
+    with gzip.open(amplicon_file, "rt") as  file:
+        seq = ""
+        
+        for line in file:
+            if not line.startswith(">"):
+                seq += line.strip()
+            if line.startswith(">"):
+                if len(seq) >= minseqlen:
+		            
+                    yield seq
+                seq = ""
+
+        if len(seq) >= minseqlen:
+    	    
+            yield seq
 
 
 def dereplication_fulllength(amplicon_file, minseqlen, mincount):
-    pass
+
+    """
+    Returns a generator of unique sequences and their occurences.
+
+    Parameters
+    ----------
+        amplicon_file : str
+            The name of the fasta file.
+
+        minseqlen : int
+            The minimum length of the sequences we want to keep.
+
+        mincount :
+            The minimum count of the sequences we want to keep.
+
+    Returns
+    -------
+        yield
+        A yield containing the sequences and their count.
+    """
+
+    seq_list = list(read_fasta(amplicon_file, minseqlen))
+    dico = Counter(seq_list)
+
+    sorted_dico = sorted(dico)
+
+    for key, value in sorted(dico.items(), key = lambda item: item[1], reverse = True):
+        if value >= mincount:
+            yield [key, value]
+
 
 def get_identity(alignment_list):
-    """Prend en une liste de séquences alignées au format ["SE-QUENCE1", "SE-QUENCE2"]
-    Retourne le pourcentage d'identite entre les deux."""
-    pass
+    
+    """
+    Calculates an identity score between two aligned sequences.
+
+    Parameters
+    ----------
+        amplicon_list :
+            A list of two aligned sequences.
+    Returns
+    -------
+        id : int
+            The identity score in %.
+    """
+
+    identical_nuc = 0
+    gap_nb = 0
+    seq1 = alignment_list[0]
+    seq2 = alignment_list[1]
+
+    for AA in range(len(seq1)):
+        if seq1[AA] == "-":
+            gap_nb += 1
+
+        if seq1[AA] != "-" or seq2[AA] != "-":
+
+            if seq1[AA] == seq2[AA]:
+                identical_nuc += 1
+
+    id = identical_nuc/len(seq1)*100
+
+    return id
 
 def abundance_greedy_clustering(amplicon_file, minseqlen, mincount, chunk_size, kmer_size):
-    pass
+    
+    """
+    Select OTUs from a fasta file containing sequences.
+
+    Parameters
+    ----------
+        amplicon_file : str
+            The name of the fasta file.
+
+        minseqlen : int
+            The minimum length of the sequences we want to keep.
+
+        mincount :
+            The minimum count of the sequences we want to keep.
+
+        chunk_size :
+            NA
+
+        kmer_sizer :
+            NA
+
+    Returns
+    -------
+        OTU_list :
+            A list containing tuples of OTUs and their counts.
+    """
+
+    candidate_list = list(dereplication_fulllength(amplicon_file, minseqlen, mincount))
+    OTU_list = [candidate_list[0]]
+
+    for seq1 in candidate_list[1:]:
+
+        id = 0
+
+        for seq2 in OTU_list:
+
+            alignment_list = nw.global_align(seq1[0], seq2[0], gap_open=-1, gap_extend=-1, matrix=os.path.abspath(os.path.join(os.path.dirname(__file__),"MATCH")))
+            id = max(id, get_identity(alignment_list))
+
+        if  id <= 97:
+            OTU_list.append(seq1)
+
+    return OTU_list
+
 
 def write_OTU(OTU_list, output_file):
-    pass
+
+    """
+    Save an OTU list in a file.
+
+    Parameters
+    ----------
+        OTU_list :
+        A list of tuples containing the contigs and their counts. 
+
+        output_file :
+        The name of the file where the results are saved.
+    """
+
+    with open(output_file, "w") as file:
+        for i, (sequence, count) in enumerate(OTU_list):
+            file.write(f">OTU_{i+1} occurrence:{count}\n{textwrap.fill(sequence, width = 80)}\n")
 
 #==============================================================
 # Main program
@@ -98,6 +241,8 @@ def main():
     # Get arguments
     args = get_arguments()
     # Votre programme ici
+    OTU_list = abundance_greedy_clustering(args.amplicon_file, args.minseqlen, args.mincount, args.chunk_size, args.kmer_size)
+    write_OTU(OTU_list, args.output_file)
 
 #==============================================================
 # Chimera removal section
